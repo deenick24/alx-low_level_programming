9c76fdbfb4d0c195db2bfe47735e9758ff518474
deenick24 <dannie.nwaba@gmail.com>
2022-09-08 17:17:47 -0700

    plow finals

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
-# !/ bin / bash
-ptintf("with proper grammar, but the outcome is a piece of art,")
+#include <studio.h>
+/**
+ *main- Serves as the entry point for the program
+ *
+ *Return: By default returns zero if no error otherwise returns non-zero value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
