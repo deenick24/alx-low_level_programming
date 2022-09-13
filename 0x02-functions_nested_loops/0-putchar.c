2c3d6e3d4793c6b82416ac8474bbd736eb70e13d
deenick24 <dannie.nwaba@gmail.com>
2022-09-13 07:17:26 -0700

    plit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,15 @@
  */
 int main(void)
 {
-	main();
+	_putchar('u');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,1 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-int main(void);
-#endif
+int _putchar(char c);
