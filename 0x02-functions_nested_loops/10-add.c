cf455700e342622babfea0e48912e3e01613f904
deenick24 <dannie.nwaba@gmail.com>
2022-09-13 16:25:17 -0700

    whisper

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @num: Caracters to add
+ * @num1: second character
+ * Return: Always return the added number
+ */
+int add(int, int)
+{
+	return (num + num1);
+}
