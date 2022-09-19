2c9a3516a1aacdcf85b6021935de9d8343bc9ca2
deenick24 <dannie.nwaba@gmail.com>
2022-09-19 04:27:00 -0700

    ursher

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - check the code
+ * @n: character to be checked
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int a;
+
+	a = 402;
+	*n = 98;
+	_putchar('0' + a);
+}
