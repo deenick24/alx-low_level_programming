9797ab736b0645225da80a689e035509cbad40f3
deenick24 <dannie.nwaba@gmail.com>
2022-09-28 04:06:28 -0700

    part of me

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion -  prints a string, followed by a new line
+ * @s: pointer to an int that will be changed
+ * Return: return void.
+ */
+void _puts_recursion(char *s)
+{
+	int str;
+
+	str = 0;
+	while (s[str] != '\0')
+	{
+		_putchar(s[str]);
+		str++;
+	}
+	_putchar('\n');
+}
+
+
